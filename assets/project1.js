abdf311c9692fce1caec8479231a0d44667ed65d
tonygnc1 <bootcamp1955@gmail.com>
2019-07-22 11:01:30 -0400

    Storing

--- assets/project1.js
+++ assets/project1.js
@@ -1,9 +1,48 @@
-var queryURL = "https://api.apixu.com/v1/history.json?key=77525317f1664f6a978165917191907&q=Charlotte&dt=2019-07-18"
+var APIkey = "77525317f1664f6a978165917191907";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    // console.log(response);
-    console.log(response.forecast.forecastday[0].hour);
-  });
+var dateStamp = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+// Capture Button Click
+$("#add-volunteer-btn").on("click", function (event) {
+
+    event.preventDefault();
+
+    var volunteer = {
+        city: $("#city-input").val(),
+        date: $("#date-input").val(),
+        hour: $("#time-input").val(),
+        weather: {},
+    }
+
+    // Clear the textbox when done
+    $("#name-input").val("");
+    $("#location-input").val("");
+    $("#time-input").val("");
+    $("#date-input").val("");
+    $("#weather").val("");
+
+    var queryURL = "https://api.apixu.com/v1/history.json?key=" + APIkey + "&q=" + volunteer.city + "&dt=" + volunteer.date;
+
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        volunteer.weather = response.forecast.forecastday[0].hour[0].condition;
+        console.log(volunteer.weather);
+        var text = response.forecast.forecastday[0].hour[0].condition.text
+        var icon = '<img src="http:' + response.forecast.forecastday[0].hour[0].condition.icon + '" alt="">'
+        $('#weather').html(text + " " + icon)
+
+    });
+});
+
+
+
+
+
+
+
